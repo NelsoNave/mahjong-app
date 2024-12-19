f148200cf554d80998ac1308f28f0fe829c37576
Risa Yamamoto <yamari2422@gmail.com>
2024-12-19 13:44:48 -0800

    refactor: redirect to the result page if users are already authorized #13

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { auth } from "@/app/lib/auth";
 import { redirect } from "next/navigation";
-import SignOut from "@/app/components/SignOut";
-import Navigation from "./components/Navigation";
 
 export default async function Home() {
   const session = await auth();
@@ -9,18 +7,7 @@ export default async function Home() {
 
   if (!session) {
     redirect("/login");
+  } else {
+    redirect("/result");
   }
-
-  // Todo : Display the dashboard when the user is authorized
-  return (
-    <>
-      <div>
-        <h1>This is your scores</h1>
-        <h2>Welcome, {session.user?.name}</h2>
-        <h2>{session.user?.image}</h2>
-      </div>
-      <SignOut />
-      <Navigation />
-    </>
-  );
 }
