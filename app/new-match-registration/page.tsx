6260d73ce0d352c4a1aba04295e6165fbb33220e
Risa Yamamoto <yamari2422@gmail.com>
2024-12-18 20:01:23 -0800

    feat: add all pages #13

---
+++ app/new-match-registration/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <>
+      <div>new match tsx</div>
+      <button>4人麻雀</button>
+      <button>3人麻雀</button>
+      <button>フリー人麻雀</button>
+    </>
+  );
+};
+
+export default page;
