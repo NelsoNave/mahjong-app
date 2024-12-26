800c7bb3013e0b1bbeb2d5024939d205e81d8410
Risa Yamamoto <yamari2422@gmail.com>
2024-12-26 13:49:14 -0800

    feat: add basic box and card components #8

---
+++ app/components/Box.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface BoxProps {
+  className?: string;
+  children: React.ReactNode;
+  size?: "sm" | "md";
+}
+
+const Box: React.FC<BoxProps> = ({ className, children, size = "md" }) => {
+  const sizeClasses = {
+    sm: {
+      height: "h-1/4",
+      width: "w-1/2",
+    },
+    md: {
+      height: "h-1/3",
+      width: "w-full",
+    },
+  };
+
+  const { height, width } = sizeClasses[size];
+
+  return (
+    <div
+      className={`${height} ${width} rounded-lg border bg-neutral-50 p-4 shadow-lg ${className}`}
+    >
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default Box;
---
+++ app/components/StatsSummaryCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const StatsSummaryCard = (props: Props) => {
+  return <div>StatsSummaryCard</div>;
+};
+
+export default StatsSummaryCard;
