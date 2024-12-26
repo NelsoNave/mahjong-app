48d5b57976544eb5eee9be6d94e9b34543b8dacd
BenOno <bentoki.1213@gmail.com>
2024-12-26 07:52:08 -0800

    chore: add query log

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -4,6 +4,54 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+});
+
+type QueryEvent = {
+  timestamp: Date;
+  query: string;
+  params: string;
+  duration: number;
+  target: string;
+};
+
+// ANSI color codes
+const colors = {
+  blue: '\x1b[34m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  reset: '\x1b[0m'
+};
+
+
+prisma.$on('query' as never, (e: QueryEvent) => {
+  if (e.query.includes("COMMIT")) {
+    return;
+  }
+  console.log(`${colors.blue}[Prisma Log] Query: ${colors.reset}` + e.query);
+  console.log(`${colors.green}[Prisma Log] Params: ${colors.reset}` + e.params);
+  console.log(`${colors.yellow}[Prisma Log] Duration: ${colors.reset}` + e.duration + 'ms');
+  
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export { prisma };
