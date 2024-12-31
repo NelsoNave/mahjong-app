34eeff8865c629357a3a054fc94dcbfc233c0c46
Risa Yamamoto <yamari2422@gmail.com>
2024-12-30 23:15:26 -0800

    feat: add the header to new page and delete the navigation component #19

--- app/(main)/new-game/[type]/page.tsx
+++ app/(main)/new-game/[type]/page.tsx
@@ -1,6 +1,6 @@
-import Navigation from '@/components/Navigation'
-import RecordForm from '@/components/RecordForm'
-import React from 'react'
+import React from "react";
+import RecordForm from "@/components/RecordForm";
+import Header from "@/components/Header";
 
 // type Props = {
 
@@ -9,14 +9,14 @@ import React from 'react'
 const page = () => {
   return (
     <>
-      <section className="flex flex-1 flex-col overflow-auto min-h-0">
+      <Header context="記録作成" />
+      <section className="flex min-h-0 flex-1 flex-col overflow-auto">
         <div className="flex flex-col justify-center gap-8 p-6">
           <RecordForm />
         </div>
       </section>
-      <Navigation />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
