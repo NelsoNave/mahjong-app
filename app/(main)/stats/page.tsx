aa9b9c047b6005f178b37e0a2d7f146584754e94
Risa Yamamoto <yamari2422@gmail.com>
2025-01-03 00:51:37 -0800

    chore: add logic to fetch available date data when rendering #8

--- app/(main)/stats/page.tsx
+++ app/(main)/stats/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Box from "@/components/Box";
 import StatsSummary from "@/components/StatsSummaryCard";
 import BalanceCard from "@/components/BalanceCard";
@@ -13,23 +13,78 @@ import { getGameStats } from "@/actions/gameStatsActions";
 import { GameStats } from "@/types/game";
 
 const Page = () => {
-  // Todo : fetch the result data
-  const [incomeData, setIncomeData] = useState([50, 75, 100, 90]);
-  const [expenseData, setExpenseData] = useState([30, 45, 60, 50]);
-  const [lineData, setLineData] = useState([20, 40, 60, 50]);
+  // dummy stats data list
+  const mockGameStats: GameStats = {
+    threePlayerGameStats: {
+      rankStats: {
+        "1": { count: 10, percentage: 33 },
+        "2": { count: 15, percentage: 50 },
+        "3": { count: 5, percentage: 17 },
+      },
+      performanceStats: {
+        winRate: 0.33,
+        averageRank: 2.1,
+        totalChips: 5000,
+        totalScore: 1500,
+      },
+      financialStats: {
+        totalIncome: 100000,
+        totalExpense: 50000,
+        totalProfit: 50000,
+        gameFee: 5000,
+        totalProfitIncludingGameFee: 45000,
+      },
+      dailyStats: [
+        { date: new Date("2024-01-01"), income: 50000, expense: 20000 },
+        { date: new Date("2024-01-02"), income: 55000, expense: 22000 },
+        { date: new Date("2024-01-03"), income: -60000, expense: 25000 },
+        { date: new Date("2024-01-04"), income: 62000, expense: 26000 },
+        { date: new Date("2024-01-05"), income: -63000, expense: 27000 },
+      ],
+    },
+    fourPlayerGameStats: {
+      rankStats: {
+        "1": { count: 10, percentage: 25 },
+        "2": { count: 15, percentage: 37.5 },
+        "3": { count: 10, percentage: 25 },
+        "4": { count: 5, percentage: 12.5 },
+      },
+      performanceStats: {
+        winRate: 55,
+        averageRank: 2.25,
+        totalChips: 4000,
+        totalScore: 1400,
+      },
+      financialStats: {
+        totalIncome: 90000,
+        totalExpense: 40000,
+        totalProfit: 50000,
+        gameFee: 4000,
+        totalProfitIncludingGameFee: 46000,
+      },
+      dailyStats: [
+        { date: new Date("2024-01-01"), income: 100000, expense: 18000 },
+        { date: new Date("2024-01-02"), income: -42000, expense: 19000 },
+        { date: new Date("2024-01-03"), income: 45000, expense: 21000 },
+        { date: new Date("2024-01-04"), income: 46000, expense: 22000 },
+        { date: new Date("2024-01-05"), income: 47000, expense: 23000 },
+      ],
+    },
+  };
+
   const {
     gameStats,
     isMonthlyView,
     setMonthView,
     setOverallView,
     targetDate,
+    isStatsLoading,
     setIsFourPlayers,
     setGameStats,
     setTargetDate,
     setAvailableDate,
+    setIsStatsLoading,
   } = useStatsStore();
-
-  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const handleMonthView = () => {
     setMonthView();
@@ -41,30 +96,30 @@ const Page = () => {
     setIsFourPlayers();
   };
 
-  useEffect(() => {
-    const fetchGameStats = async () => {
-      try {
-        const result = await getGameStats(isMonthlyView ? targetDate : "");
+  const fetchGameStats = async () => {
+    if (!targetDate) return;
+    setIsStatsLoading(true);
 
-        if (result.status === "success") {
-          alert("データの取得に成功しました");
-          if (result.data) {
-            setGameStats(result.data);
-          }
-        } else {
-          alert(result.message);
-        }
-      } catch (err) {
-        console.error(err);
+    try {
+      const statsData = await getGameStats(isMonthlyView ? targetDate : "");
+      if (statsData.status === "success" && statsData.data) {
+        setGameStats(statsData.data);
+      } else {
+        alert(statsData.message);
       }
-    };
-
-    fetchGameStats();
-  }, [targetDate]);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsStatsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    setIsLoading(true);
-    // TODO : server actionsで日付一覧を取得
+    if (targetDate) return;
+    setIsStatsLoading(true);
+
+    // TODO: fetch available date data
+    // dummy data
     const availableDate: string[] = [
       "2024-01",
       "2024-05",
@@ -73,60 +128,65 @@ const Page = () => {
       "2024-12",
     ];
 
-    const latestMonth = availableDate[availableDate.length - 1];
     setAvailableDate(availableDate);
+
+    // get the latest month
+    const latestMonth = availableDate[availableDate.length - 1];
     setTargetDate(latestMonth);
 
-    setIsLoading(false);
+    setIsStatsLoading(false);
   }, []);
 
-  if (isLoading) {
-    return (
-      <div className="flex h-full w-full items-center justify-center">
-        <div>Loading...</div>
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (!targetDate) return;
+    fetchGameStats();
+  }, [targetDate, isMonthlyView]);
 
   return (
-    <div className="bg-background">
-      <Header context="成績" />
-      <div className="flex flex-col justify-between gap-2 px-6 py-2">
-        <div className="flex w-[128px] justify-between rounded-xl bg-lightPineGlade px-2 py-1 text-black">
-          <button
-            className={`${
-              isMonthlyView ? "bg-pineGlade font-semibold" : ""
-            } rounded-lg px-3 transition-all duration-300`}
-            onClick={handleMonthView}
-          >
-            月別
-          </button>
-          <button
-            className={`${
-              !isMonthlyView ? "bg-pineGlade font-semibold" : ""
-            } rounded-lg px-3 transition-all duration-300`}
-            onClick={handleOverallView}
-          >
-            総合
-          </button>
-        </div>
-        {isMonthlyView && <MonthScroll />}
-        <div className="flex flex-col gap-1">
-          <Box>
-            <ChartComponent gameStats={mockGameStats} />
-          </Box>
-          <Box>
-            <StatsSummary gameStats={mockGameStats} />
-          </Box>
-          <div className="flex h-full gap-1">
-            <Box size="sm">
-              <BalanceCard gameStats={mockGameStats} />
-            </Box>
-            <Box size="sm">
-              <TotalCostResultCard gameStats={mockGameStats} />
-            </Box>
+    <div className="flex-1 overflow-y-auto">
+      <div className="sticky top-0 z-50 bg-background">
+        <Header context="成績" />
+      </div>
+      <div
+        className={`${isStatsLoading && "h-full items-center justify-center"} flex flex-col overflow-y-auto bg-background`}
+      >
+        {isStatsLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="flex min-h-screen flex-col gap-2 border px-6 py-4">
+            <div className="flex w-[136px] justify-between rounded-xl bg-lightPineGlade px-2 py-[5px] text-black">
+              <button
+                className={`${isMonthlyView ? "bg-pineGlade font-semibold" : ""} rounded-lg px-3 py-[2px] transition-all duration-300`}
+                onClick={handleMonthView}
+              >
+                月別
+              </button>
+              <button
+                className={`${!isMonthlyView ? "bg-pineGlade font-semibold" : ""} rounded-lg px-3 py-[2px] transition-all duration-300`}
+                onClick={handleOverallView}
+              >
+                総合
+              </button>
+            </div>
+            {isMonthlyView && <MonthScroll />}
+            <div className="flex flex-col gap-2">
+              <Box>
+                <ChartComponent gameStats={mockGameStats} />
+              </Box>
+              <Box>
+                <StatsSummary gameStats={mockGameStats} />
+              </Box>
+              <div className="flex gap-2">
+                <Box size="sm">
+                  <BalanceCard gameStats={mockGameStats} />
+                </Box>
+                <Box size="sm">
+                  <TotalCostResultCard gameStats={mockGameStats} />
+                </Box>
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- app/store/useStatsStore.ts
+++ app/store/useStatsStore.ts
@@ -7,6 +7,7 @@ interface StatsStore {
   gameStats: GameStats | null;
   targetDate: string;
   availableDate: string[];
+  isStatsLoading: boolean;
   setMonthView: () => void;
   setOverallView: () => void;
   setIsFourPlayers: () => void;
@@ -14,6 +15,7 @@ interface StatsStore {
   setGameStats: (gameStats: GameStats) => void;
   setTargetDate: (targetDate: string) => void;
   setAvailableDate: (newDates: string[]) => void;
+  setIsStatsLoading: (loadState: boolean) => void;
 }
 
 export const useStatsStore = create<StatsStore>()((set) => ({
@@ -22,6 +24,7 @@ export const useStatsStore = create<StatsStore>()((set) => ({
   gameStats: null,
   targetDate: "",
   availableDate: [],
+  isStatsLoading: false,
   setMonthView: () => set({ isMonthlyView: true }),
   setOverallView: () => set({ isMonthlyView: false }),
   setIsFourPlayers: () => set({ isFourPlayers: true }),
@@ -35,4 +38,6 @@ export const useStatsStore = create<StatsStore>()((set) => ({
       ...state,
       availableDate: [...state.availableDate, ...newDates],
     })),
+  setIsStatsLoading: (loadState: boolean) =>
+    set(() => ({ isStatsLoading: loadState })),
 }));
