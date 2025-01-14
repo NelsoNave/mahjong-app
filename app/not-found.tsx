09cff4632675a8f005088a3d626204bc67b0db96
Risa Yamamoto <yamari2422@gmail.com>
2025-01-14 08:10:23 -0800

    refactor: delete the Button component #47

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Button = ({
-  children,
-  href,
-}: {
-  children: React.ReactNode;
-  href: string;
-}) => {
-  return (
-    <Link
-      href={href}
-      className="transform rounded-lg border border-amazon bg-white px-6 py-3 transition-all duration-300 ease-in-out hover:scale-105"
-    >
-      {children}
-    </Link>
-  );
-};
-
 export default function NotFound() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-8 px-4 py-8">
@@ -31,7 +14,12 @@ export default function NotFound() {
         height={200}
         className="mb-6 h-48 w-48 object-contain"
       />
-      <Button href="/">トップに戻る</Button>
+        <Link
+          href="/"
+          className="transform rounded-lg border border-amazon bg-white px-6 py-3 transition-all duration-300 ease-in-out hover:scale-105"
+        >
+          トップに戻る
+        </Link>
     </div>
   );
 }
